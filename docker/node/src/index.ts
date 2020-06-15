a756a46c059b3c410df75f13ca91d83322c81b7a
和田健嗣 <ponporopen@gmail.com>
2020-06-15 22:35:42 +0900

    index.tsファイル分割

--- docker/node/src/index.ts
+++ docker/node/src/index.ts
@@ -1,75 +1,10 @@
-import {ApolloServer, gql, AuthenticationError} from 'apollo-server-micro'
-import models from './db/models'
+import {ApolloServer} from 'apollo-server-micro'
 import * as jwt from 'jsonwebtoken'
 
-const {staff, login} = models
+import resolvers from './resolvers'
+import typeDefs from './typeDefs'
 
-const typeDefs = gql`
-    type Staff {
-        id: ID!
-        facilityId: Int
-        firstName: String
-        lastName: String
-        facilityAdmin: Boolean
-        corporationAdmin: Boolean
-        createdAt: String
-        updatedAt: String
-    }
-    type Tokens {
-        accessToken: String
-    }
-    type Me {
-        id: Int
-    }
-    type Query {
-        sayHello(name: String): String
-        getStaffs: [Staff]
-        getMe: Me
-    }
-    type Mutation {
-        login(username: String!
-            password: String!): Tokens
-    }
-`
-
-const resolvers = {
-  Query: {
-    sayHello: (_parent: any, args: {name: string}) => {
-      return `Hello ${args.name}!`
-    },
-    getStaffs: async () => {
-      return staff.findAll()
-    },
-    getMe: async (_parent: any, _args: any, context: any) => {
-      if (!context.me) throw new AuthenticationError(context.authenticationError)
-      return context.me
-    }
-  },
-  Mutation: {
-    login: async (_parent: any, args: {username: string, password: string}) => {
-      const staffRow = await staff.findOne({
-        include: [{
-          model: login,
-          where: {username: args.username}
-        }]
-      })
-
-      if (!staffRow) throw new AuthenticationError('invalid username')
-
-      const {id, login: {password}} = staffRow
-
-      if (password !== args.password) throw new AuthenticationError('invalid password')
-
-      const accessToken = jwt.sign(
-        {
-          id: id,
-        }, 'dummysecret'
-      )
-
-      return {accessToken}
-    }
-  }
-}
+const CONFIG_AUTH = require('./config/authentication.json')
 
 const context = async ({req}: {req: any}) => {
   if (!req.headers['authorization']) return {
@@ -78,7 +13,7 @@ const context = async ({req}: {req: any}) => {
   const token = req.headers['authorization'].split(' ')[1]
   try {
     return {
-      me: jwt.verify(token, 'dummysecret')
+      me: jwt.verify(token, CONFIG_AUTH.ACCESS_TOKEN_SECRET)
     }
   } catch (e) {
     console.log(e)
