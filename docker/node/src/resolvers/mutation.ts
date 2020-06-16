5df0e768e4b6847ba6a52850f4600a35d21b2ea3
和田健嗣 <ponporopen@gmail.com>
2020-06-16 15:31:06 +0900

    resolvers修正追加等

--- docker/node/src/resolvers/mutation.ts
+++ docker/node/src/resolvers/mutation.ts
@@ -1,29 +1,27 @@
 import {AuthenticationError} from "apollo-server-errors"
 import * as jwt from "jsonwebtoken"
 
-const CONFIG_AUTH = require('../config/authentication.json')
+import {findStaffByLoginUsername} from '../models/database'
 
-import models from '../db/models'
-const {staff, login} = models
+const CONFIG_AUTH = require('../config/authentication.json')
 
 export default {
   login: async (_parent: any, args: {username: string, password: string}) => {
-    const staffRow = await staff.findOne({
-      include: [{
-        model: login,
-        where: {username: args.username}
-      }]
-    })
+    const staffRow = await findStaffByLoginUsername(args.username)
 
     if (!staffRow) throw new AuthenticationError('invalid username')
 
-    const {id, login: {password}} = staffRow
+    const {id, facilityId, facilityAdmin, corporationAdmin, systemAdmin, login: {password}} = staffRow
 
     if (password !== args.password) throw new AuthenticationError('invalid password')
 
     const accessToken = jwt.sign(
       {
-        id: id,
+        id,
+        facilityId,
+        facilityAdmin,
+        corporationAdmin,
+        systemAdmin
       }, CONFIG_AUTH.ACCESS_TOKEN_SECRET
     )
 
--- docker/node/src/resolvers/query.ts
+++ docker/node/src/resolvers/query.ts
@@ -1,17 +1,28 @@
 import {AuthenticationError} from "apollo-server-errors"
-import models from '../db/models'
-
-const {staff} = models
+import * as models from '../models/database'
 
 export default {
   sayHello: (_parent: any, args: {name: string}) => {
     return `Hello ${args.name}!`
   },
   getStaffs: async () => {
-    return staff.findAll()
+    return models.fetchStaffs()
   },
   getMe: async (_parent: any, _args: any, context: any) => {
     if (!context.me) throw new AuthenticationError(context.authenticationError)
     return context.me
+  },
+  getUsers: async (_parent: any, _args: any, context: any) => {
+    if (!context.me) throw new AuthenticationError(context.authenticationError)
+    const {facilityId, corporationAdmin, systemAdmin} = context.me
+
+    if (systemAdmin) return models.fetchUsers()
+
+    if (corporationAdmin) {
+      const {corporationId} = await models.findFacility(facilityId)
+      return models.fetchUsersByCorporationId(corporationId)
+    }
+
+    return models.fetchUsersByFacilityId(facilityId)
   }
 }
