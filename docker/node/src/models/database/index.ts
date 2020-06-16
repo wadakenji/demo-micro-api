e4d484bb571000dfb688311bc5830e4f463fff9d
和田健嗣 <ponporopen@gmail.com>
2020-06-16 15:30:43 +0900

    modelsディレクトリ作成

---
+++ docker/node/src/models/database/index.ts
@@ -0,0 +1,42 @@
+import models from '../../db/models'
+
+export const findStaffByLoginUsername = async (username: string) => {
+  return models.staff.findOne({
+    include: [{
+      model: models.login,
+      where: {username}
+    }]
+  })
+}
+
+export const fetchStaffs = async () => {
+  return models.staff.findAll()
+}
+
+export const fetchUsers = async () => {
+  return models.user.findAll()
+}
+
+export const fetchUsersByFacilityId = async (facilityId: number) => {
+  return models.user.findAll({
+    include: [{
+      model: models.facility,
+      where: {id: facilityId}
+    }]
+  })
+}
+
+export const fetchUsersByCorporationId = async (corporationId: number) => {
+  return models.user.findAll({
+    include: [{
+      model: models.facility,
+      where: {corporationId},
+    }]
+  })
+}
+
+export const findFacility = async (facilityId: number) => {
+  return models.facility.findOne({
+    where: {id: facilityId}
+  })
+}
