b3f00f81ede6cab72f5463f7b252ccf4fe8ed88b
和田健嗣 <ponporopen@gmail.com>
2020-06-17 14:38:05 +0900

    websocketサーバーを建てれるようにindex.ts改修・subscriptionServer.ts追加

--- docker/node/src/index.ts
+++ docker/node/src/index.ts
@@ -1,16 +1,21 @@
-import {ApolloServer} from 'apollo-server-micro'
+import {ApolloServer, makeExecutableSchema} from 'apollo-server-micro'
 import * as jwt from 'jsonwebtoken'
+import {send} from 'micro'
+import {get, post, router} from 'microrouter'
 
 import resolvers from './resolvers'
 import typeDefs from './schemas'
 
 const CONFIG_AUTH = require('./config/authentication.json')
 
+export const schema = makeExecutableSchema({typeDefs, resolvers})
+
+//contextにjwt認証結果を格納
 const context = async ({req}: {req: any}) => {
-  if (!req.headers['authorization']) return {
+  if (!req.headers.authorization) return {
     authenticationError: 'no token'
   }
-  const token = req.headers['authorization'].split(' ')[1]
+  const token = req.headers.authorization.split(' ')[1]
   try {
     return {
       me: jwt.verify(token, CONFIG_AUTH.ACCESS_TOKEN_SECRET)
@@ -23,5 +28,16 @@ const context = async ({req}: {req: any}) => {
   }
 }
 
-const apolloServer = new ApolloServer({typeDefs, resolvers, context})
-module.exports = apolloServer.createHandler()
+const graphqlPath = '/graphql'
+const apolloServer = new ApolloServer({schema, context})
+const graphqlHandler = apolloServer.createHandler({path: graphqlPath})
+
+const app = router(
+  get('/', () => 'hello, Micro'),
+  get('/about', () => 'about page'),
+  post(graphqlPath, graphqlHandler),
+  get(graphqlPath, graphqlHandler),
+  (_, res) => send(res, 404, 'Not Found'),
+)
+
+export default app
