d093452da9e2f43ee47a926acb4fc0ec2101ac27
和田健嗣 <ponporopen@gmail.com>
2020-06-19 19:12:25 +0900

    サブスクリプションのjwt認証

--- docker/node/src/index.ts
+++ docker/node/src/index.ts
@@ -1,4 +1,5 @@
-import {ApolloServer, makeExecutableSchema} from 'apollo-server'
+import {ApolloServer, makeExecutableSchema, gql} from 'apollo-server'
+import {AuthenticationError} from 'apollo-server-errors'
 import * as jwt from 'jsonwebtoken'
 
 import resolvers from './resolvers'
@@ -8,16 +9,11 @@ const CONFIG_AUTH = require('./config/authentication.json')
 
 export const schema = makeExecutableSchema({typeDefs, resolvers})
 
-const context = async ({req, connection}: {req: any, connection: any}) => {
-
-  //subscription context
-  if (connection) return connection.context
-
-  //jwt情報を格納
-  if (!req.headers.authorization) return {
+const verifyToken = (authorizationHeader: string) => {
+  if (!authorizationHeader) return {
     authenticationError: 'no token'
   }
-  const token = req.headers.authorization.split(' ')[1]
+  const token = authorizationHeader.split(' ')[1]
   try {
     return {
       me: jwt.verify(token, CONFIG_AUTH.ACCESS_TOKEN_SECRET)
@@ -30,7 +26,31 @@ const context = async ({req, connection}: {req: any, connection: any}) => {
   }
 }
 
-const apolloServer = new ApolloServer({schema, context})
+const subscriptions = {
+  //サブスクリプションを始める（openingハンドシェイク）ときに発火する
+  //connectionParamsにはリクエストヘッダーの一部が格納されている（多分）
+  onConnect: (connectionParams: any) => {
+    const authContext = verifyToken(connectionParams.authorization)
+    //認証エラーとなっていればエラーレスポンスを返す
+    if (authContext.authenticationError) throw new AuthenticationError(authContext.authenticationError)
+    //認証パスすれば下のcontextとネームしたコールバックを経由して（引数connectionに格納される）jwtの情報をリゾルバーにわたす
+    return authContext
+  }
+}
+
+const context = async ({req, connection}: {req: any, connection: any}) => {
+  //サブスクリプションのときはconnectionがtruthyな値になる
+  if (connection) return connection.context
+
+  //jwt情報を格納
+  return verifyToken((req.headers.authorization))
+}
+
+const apolloServer = new ApolloServer({
+  schema,
+  context,
+  subscriptions
+})
 
 apolloServer.listen({port: 3000}).then(({url}) => {
   console.log(`Server listening on ${url}`)
